a969207baf190f50593d83bd8c321e391b420b48
Mahmoodaghe <mahmoodaghe@gmail.com>
2024-09-26 18:02:27 +0300

    Add files via upload

---
+++ المحاضرة الثالثة عملي/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace المحاضرة_الثالثة_عملي
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fristN = new System.Windows.Forms.TextBox();
+            this.secondN = new System.Windows.Forms.TextBox();
+            this.opertion2 = new System.Windows.Forms.TextBox();
+            this.opertion1 = new System.Windows.Forms.TextBox();
+            this.thirdN = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fristN
+            // 
+            this.fristN.Location = new System.Drawing.Point(53, 24);
+            this.fristN.Name = "fristN";
+            this.fristN.Size = new System.Drawing.Size(100, 20);
+            this.fristN.TabIndex = 0;
+            this.fristN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.fristN_KeyPress);
+            // 
+            // secondN
+            // 
+            this.secondN.Location = new System.Drawing.Point(53, 97);
+            this.secondN.Name = "secondN";
+            this.secondN.Size = new System.Drawing.Size(100, 20);
+            this.secondN.TabIndex = 1;
+            // 
+            // opertion2
+            // 
+            this.opertion2.Location = new System.Drawing.Point(53, 138);
+            this.opertion2.Name = "opertion2";
+            this.opertion2.Size = new System.Drawing.Size(100, 20);
+            this.opertion2.TabIndex = 2;
+            // 
+            // opertion1
+            // 
+            this.opertion1.Location = new System.Drawing.Point(53, 60);
+            this.opertion1.Name = "opertion1";
+            this.opertion1.Size = new System.Drawing.Size(100, 20);
+            this.opertion1.TabIndex = 3;
+            this.opertion1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.opertion1_KeyPress);
+            // 
+            // thirdN
+            // 
+            this.thirdN.Location = new System.Drawing.Point(53, 184);
+            this.thirdN.Name = "thirdN";
+            this.thirdN.Size = new System.Drawing.Size(100, 20);
+            this.thirdN.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "حساب";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "اغلاق";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "العدد الاول";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "العملية";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(171, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "العدد الثاني";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(171, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "العملية";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(171, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "العدد الثالث";
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(53, 228);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 20);
+            this.result.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(171, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "الناتج";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 309);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.thirdN);
+            this.Controls.Add(this.opertion1);
+            this.Controls.Add(this.opertion2);
+            this.Controls.Add(this.secondN);
+            this.Controls.Add(this.fristN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fristN;
+        private System.Windows.Forms.TextBox secondN;
+        private System.Windows.Forms.TextBox opertion2;
+        private System.Windows.Forms.TextBox opertion1;
+        private System.Windows.Forms.TextBox thirdN;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ المحاضرة الثالثة عملي/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace المحاضرة_الثالثة_عملي
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { //تضعيف الاحداث للعدد الثاني والثالث
+            secondN.KeyPress += fristN_KeyPress;
+            thirdN.KeyPress += fristN_KeyPress;
+            //تضعيف الداله التي تقوم بفحص العملية 
+            opertion2.KeyPress += opertion1_KeyPress;
+          }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number1, number2, number3;
+            string op1 = opertion1.Text;
+            string op2 = opertion2.Text;
+            string fristRoust;
+            string fainalRoust;
+
+            if (double.TryParse(fristN.Text, out number1) &&
+                double.TryParse(secondN.Text, out number2) &&
+                double.TryParse(thirdN.Text, out number3))
+            {//فحص العمليات الاولا قي العملية الاولا
+                if ((op1 == "/" || op1 == "*") || op1==op2)  //الاولويه للعملية / و * ثم اذا كانت العمليات متساوية نفذ من هذا الاتجاه
+                {
+                    fristRoust = sumNumber(number1, number2, op1).ToString();
+                    if (fristRoust == "NaN") 
+                    {
+                        MessageBox.Show("غلط في العمليه الاولى");
+                        opertion1.Focus();
+                        return;
+                    }
+
+                    fainalRoust = sumNumber(Convert.ToDouble(fristRoust), number3, op2).ToString();
+                    if (fainalRoust == "NaN")
+                    {
+                        MessageBox.Show("العملية الثانية غلط");
+                        opertion2.Focus();
+                        return;
+                    }
+
+                    result.Text = fainalRoust.ToString();
+
+                }
+                    //فحص العملية الثانية اذا كنت هيا الاولا للعملية الحسابيه
+                else if ((op2 == "*" || op2 == "/") )
+                {
+                    fristRoust = sumNumber(number2, number3, op2).ToString();
+                    if (fristRoust == "NaN")
+                    {
+                        MessageBox.Show("العملية الثانية غلط");
+                        opertion2.Focus();
+                        return;
+                    }
+                    fainalRoust = sumNumber(Convert.ToDouble(fristRoust), number1, op1).ToString();
+                    if (fainalRoust == "NaN")
+                    {
+                        MessageBox.Show("العملية الاوللى غلط");
+                        opertion1.Focus();
+                        return;
+                    }
+                    result.Text = fainalRoust.ToString();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("يرجو ادخال قيم صحيحة");
+            }
+            }
+        //الداله التي تقوم بالعملية الحسابية
+            private double sumNumber(double n1,double n2,string op)
+            {
+                    switch (op)
+                    { case "+":
+                            return n1+n2;
+                        case "-":
+                            return n1-n2;
+                        case "*":
+                            return n1*n2;
+                        case "/":
+                            {
+                                if (n2 == 0)
+                                    return 0;
+                                else
+                                    return n1 / n2;
+                            }
+                        default:
+                            return double.NaN;
+                    }
+
+            }
+            private void fristN_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                //دالة لاتقبل الحروف 
+                if ((e.KeyChar < 48 || e.KeyChar > 57) && (e.KeyChar != 8) && e.KeyChar!=46)
+                    e.Handled = true;
+            }
+
+            private void opertion1_KeyPress(object sender, KeyPressEventArgs e)
+            {// لا تقبل في خانات العملية الا عملية الجمع والضرب والطرح والقسمة فقط
+                if (e.KeyChar != 43 && e.KeyChar != 42 && e.KeyChar != 45 && e.KeyChar != 47 && e.KeyChar!=8)
+                    e.Handled = true;
+            }
+
+
+        }
+    }
+
